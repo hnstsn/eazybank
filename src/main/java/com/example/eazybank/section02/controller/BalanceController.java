8c8d1fb2e908fde58974bb279a0e8c4a2248f2ae
WANSEO\hnsts <hnstsn@naver.com>
2024-04-16 22:54:38 +0900

    eazy bank project start

---
+++ src/main/java/com/example/eazybank/section02/controller/BalanceController.java
@@ -0,0 +1,9 @@
+package com.example.eazybank.section02.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BalanceController {
+
+
+}
---
+++ src/main/java/com/example/eazybank/section02/controller/ContactController.java
@@ -0,0 +1,7 @@
+package com.example.eazybank.section02.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContactController {
+}
